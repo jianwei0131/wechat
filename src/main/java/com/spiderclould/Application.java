188549e35268e4133587266113cdc74f14307b37
jianwei0131 <jianwei0131@qq.com>
2019-11-26 12:43:34 +0800

    test

--- src/main/java/com/spiderclould/Application.java
+++ src/main/java/com/spiderclould/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
- * 
+ * test
  * @author David Wang
  * 2019-11-26
  */
