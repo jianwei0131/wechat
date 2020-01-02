1ef654954a45289332023829010fcb6e18dedde5
jianwei0131 <5425254+jianwei0131@users.noreply.github.com>
2020-01-02 10:04:49 +0800

    u

---
+++ src/main/java/com/spiderclould/mybatis/typehandler/LongVarcharTypeHandler.java
@@ -0,0 +1,43 @@
+package com.spiderclould.mybatis.typehandler;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+/**
+ * 
+ * @author David Wang
+ * 2019-06-06
+ */
+@MappedJdbcTypes({JdbcType.LONGVARCHAR, JdbcType.LONGNVARCHAR})
+@MappedTypes(value = String.class)
+public class LongVarcharTypeHandler extends BaseTypeHandler<String>  {
+
+	@Override
+	public void setNonNullParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType)
+			throws SQLException {
+		 ps.setString(i,parameter);
+	}
+
+	@Override
+	public String getNullableResult(ResultSet rs, String columnName) throws SQLException {
+		return rs.getString(columnName);
+	}
+
+	@Override
+	public String getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+		return rs.getString(columnIndex);
+	}
+
+	@Override
+	public String getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+		return cs.getString(columnIndex);
+	}
+
+}
